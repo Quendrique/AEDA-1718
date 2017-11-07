7a024e2dc29b6be9416d2041708450d19f7fa7e6
catarinafigueiredo <32490017+catarinafigueiredo@users.noreply.github.com>
2017-11-07 22:19:35 +0000

    Update oferta.cpp
    funções base

--- oferta.cpp
+++ oferta.cpp
@@ -1,5 +1,79 @@
 #include "oferta.h"
-
+#include <fstream>
+#include <sstream>
 Oferta::Oferta()
 {
+	
+}
+Oferta::Oferta(string barco, unsigned int lotacaoMax, string destino, Data data)
+{
+	this->barco = barco;
+	this->lotacaoMax = lotacaoMax;
+	this->destino = destino;
+	// falta a data
+}
+string Oferta::getBarco() const
+{
+	return barco;
+}
+unsigned int Oferta::getLotacaoMax() const
+{
+	return lotacaoMax;
+}
+string Oferta::getDestino() const
+{
+	return destino;
+}
+unsigned int Oferta::getId() const
+{
+	return id;
+}
+unsigned int Oferta::getLotacao() const
+{
+	return lotacao;
+}
+void Oferta::setBarco(string barco)
+{
+	this->barco = barco;
+}
+void Oferta::setLotacaoMax(unsigned int lotacaoMax)
+{
+	this->lotacaoMax = lotacaoMax;
+}
+void Oferta::setDestino(string destino)
+{
+	this->destino = destino;
+}
+bool Oferta::operator ==(const Oferta * o2)
+{
+	if (this->barco == o2->barco)
+	{
+		if (this->destino == o2->destino)
+		{
+			if (this->lotacaoMax == o2->lotacaoMax)
+			{
+				if (this->distancia == o2->distancia)
+				{
+					if (this->data.getMes() == o2->data.getMes())
+					{
+						if (this->data.getDia() == o2->data.getDia())
+						{
+							if (this->data.getHoraInicio() == o2->data.getHoraInicio())
+							{
+								if (this->data.getHoraFim() == o2->data.getHoraFim())
+								{
+									return true;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
+void Oferta::addToLotacao()
+{
+	this->lotacao = lotacao + 1;
 }
