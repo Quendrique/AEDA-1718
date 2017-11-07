3c1a4be93d6beb8d2ab6739514f9c628792a5507
catarinafigueiredo <32490017+catarinafigueiredo@users.noreply.github.com>
2017-11-07 22:21:09 +0000

    funções base data

--- data.cpp
+++ data.cpp
@@ -1,2 +1,31 @@
+#include "data.h"
 
+
+Data::Data()
+{
+
+}
+Data::Data(unsigned int dia, unsigned int mes, unsigned int horaInicio, unsigned int horaFim)
+{
+	this->dia = dia;
+	this->mes = mes;
+	this->horaInicio = horaInicio;
+	this->horaFim = horaFim;
+}
+unsigned int Data::getDia() const
+{
+	return dia;
+}
+unsigned int Data::getMes() const
+{
+	return mes;
+}
+unsigned int Data::getHoraInicio() const
+{
+	return horaInicio;
+}
+unsigned int Data::getHoraFim() const
+{
+	return horaFim;
+}
 
