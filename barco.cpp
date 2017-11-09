0e223cf5deb890e1dc94d8d9f568cc9ed43876dd
Quendrique <13695072+Quendrique@users.noreply.github.com>
2017-11-09 11:50:27 +0000

    Accidentally erased code in barco.cpp, already corrected

--- barco.cpp
+++ barco.cpp
@@ -3,3 +3,19 @@
 Barco::Barco()
 {
 }
+
+Barco::Barco(string tipo, unsigned int lotacaoMax)
+{
+	this->tipo = tipo;
+	this->lotacaoMax = lotacaoMax;
+}
+
+void Barco::setTipo(string tipo)
+{
+	this->tipo = tipo;
+}
+
+void Barco::setLotacaoMax(unsigned int lotacaoMax)
+{
+	this->lotacaoMax = lotacaoMax;
+}
