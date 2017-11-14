c197a4d761373952249f5d3a917450cfdfa7825a
catarinafigueiredo <32490017+catarinafigueiredo@users.noreply.github.com>
2017-11-14 11:51:36 +0000

    added minutosInicio, minutosFim

--- data.cpp
+++ data.cpp
@@ -2,26 +2,58 @@
 
 Data::Data() {}
 
-Data::Data(unsigned int dia, unsigned int mes, unsigned int horaInicio, unsigned int horaFim) {
+Data::Data(unsigned int dia, unsigned int mes, unsigned int horaInicio, unsigned int horaFim, unsigned int minutosInicio, unsigned int minutosFim) {
 	this->dia = dia;
 	this->mes = mes;
 	this->horaInicio = horaInicio;
 	this->horaFim = horaFim;
+	this->minutosInicio = minutosInicio;
+	this->minutosFim = minutosFim;
 }
 
-unsigned int Data::getDia() const {return dia;}
+unsigned int Data::getMinutosInicio() const{return minutosInicio;}
 
-unsigned int Data::getMes() const {return mes;}
+unsigned int Data::getMinutosFim() const{ return minutosFim; }
 
-unsigned int Data::getHoraInicio() const {return horaInicio;}
+unsigned int Data::getDia() const { return dia; }
 
-unsigned int Data::getHoraFim() const {return horaFim;}
+unsigned int Data::getMes() const { return mes; }
+
+unsigned int Data::getHoraInicio() const { return horaInicio; }
+
+unsigned int Data::getHoraFim() const { return horaFim; }
 
 string Data::printData() const {
 
 	std::ostringstream os;
-	os << dia << "/" << mes << " " << horaInicio << "->" << horaFim;
+	os << dia << "/" << mes << " " << horaInicio<<":"<< minutosInicio << "->" << horaFim<<":"<< minutosFim;
 	return os.str();
 
 }
+void Data::setDia(unsigned int dia)
+{
+	this->dia=dia;
+}
+void Data::setMes(unsigned int mes)
+{
+	this->mes=mes;
+}
+void Data::setHoraInicio(unsigned int horaInicio)
+{
+	this->horaInicio=horaInicio;
+}
+void Data::setHoraFim(unsigned int horaFim)
+{
+	this->horaFim=horaFim;
+}
 
+void Data::setMinutosInicio(unsigned int horaInicio)
+{
+	this->minutosInicio=minutosInicio;
+
+}
+void Data::setMinutosFim(unsigned int horaFim)
+{
+	this->minutosFim=minutosFim;
+
+}
