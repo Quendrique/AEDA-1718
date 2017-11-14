36b055f8f99498d7b3591ddc4e6ca8f792c13090
catarinafigueiredo <32490017+catarinafigueiredo@users.noreply.github.com>
2017-11-14 11:52:30 +0000

    added minutosInicio, minutosFim

--- data.h
+++ data.h
@@ -14,19 +14,25 @@ private:
 	unsigned int dia;
 	unsigned int mes;
 	unsigned int horaInicio;
+	unsigned int minutosInicio;
 	unsigned int horaFim;
+	unsigned int minutosFim;
 
 public:
 	Data();
-	Data(unsigned int dia, unsigned int mes, unsigned int horaInicio, unsigned int horaFim);
+	Data(unsigned int dia, unsigned int mes, unsigned int horaInicio, unsigned int horaFim,unsigned int minutosInicio, unsigned int minutosFim);
 	void setDia(unsigned int dia);
 	void setMes(unsigned int mes);
 	void setHoraInicio(unsigned int horaInicio);
 	void setHoraFim(unsigned int horaFim);
+	void setMinutosInicio(unsigned int horaInicio);
+	void setMinutosFim(unsigned int horaFim);
 	unsigned int getDia() const;
 	unsigned int getMes() const;
 	unsigned int getHoraInicio() const;
 	unsigned int getHoraFim() const;
+	unsigned int getMinutosInicio() const;
+	unsigned int getMinutosFim() const;
 	string printData() const;
 
 };
