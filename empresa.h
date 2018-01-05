4573bc916efe0500e6d600c186b0b79f1a325685
catarinafigueiredo <32490017+catarinafigueiredo@users.noreply.github.com>
2018-01-05 13:10:46 +0000

    comentarios adicionados não completo

--- empresa.h
+++ empresa.h
@@ -45,44 +45,169 @@ private:
 	string nome;
 
 public:
+	/**
+	*	@brief Construtor de uma empresa
+	*   @param fichFornecedores ficheiro com os clientes
+	*   @param fichOferta ficheiro com as ofertas
+	*   @param fichClientes ficheiro com os clientes
+	*   @param fichClientesReg ficheiro com os clientes registados 
+	*   @param data atual 
+	*/
     Empresa(string fichFornecedores, string fichOfertas, string fichClientes, string fichClientesReg, string fichReservas, Data data_Atual); // alterei
+	/**
+	*	@brief Construtor de uma empresa
+	*   @param nome
+	*   @param fornecedores
+	*   @param clientes
+	*/
 	Empresa(string nome, vector<Fornecedor> fornecedores, vector<Cliente*> clientes);
-	
+	/**
+	*	@brief Adiciona um fornecedor
+	*   @param fornecedor 
+	*/
 	void addFornecedor(Fornecedor fornecedorNew);
+	/**
+	*	@brief Adiciona um cliente
+	*   @param cliente
+	*/
 	void addCliente(Cliente * clienteNew);
+	/**
+	*	@brief Adiciona uma oferta a um fornecedor
+	*   @param NIF
+	*/
 	void addOfertas(unsigned int NIF);
+	
 	void removeFornecedor(unsigned long fornecedorRemoveNIF);
 	void removeCliente(unsigned long clienteRemoveNIF);
 	void removerOferta(Oferta o1);
 	bool compareData(Data data_cliente); // alterei
 	Cliente * getCliente() const;
+	/**
+	*	@brief Devolve um vetor com fornecedores
+	*/
 	vector<Fornecedor> getFornecedores() const;
+	/**
+	*	@brief Devolve o nome da empresa
+	*/
 	string getNome () const;
+	
 	void setData_atual(int dia, int mes);// acrescentei
+	/**
+	*	@brief Carrega os fornecedores a partir de um ficheiro
+	*   @param fichFornecedor ficheiro a utilizar 
+	*/
 	void carregaFornecedores(string fichFornecedor); //extrai o conteudo do ficheiro de fornecedores
+	/**
+	*	@brief Carrega as ofertas a partir de um ficheiro
+	*   @param ficheiro_oferta ficheiro a utilizar
+	*/
 	void carregaOferta( string ficheiro_oferta);//extrai o conteudo do ficheiro de ofertas
+	/**
+	*	@brief Carrega os clientes a partir de um ficheiro
+	*   @param fichClientes ficheiro a utilizar
+	*/
 	void carregaClientes(string fichClientes);
+	/**
+	*	@brief Carrega os clientes registados a partir de um ficheiro
+	*   @param fichClientes ficheiro a utilizar
+	*/
 	void carregaClientesReg(string fichClientesR);
+	
 	void carregaReservas(string ficheiro_reservas);
+	/**
+	*	@brief Imprime as ofertas de um fornecedor
+	*   @param NIF do fornecedor
+	*/
 	void visualizaOfertas(unsigned long NIF);
-
+/**
+	*	@brief Guarda os fornecedores num ficheiro
+	*   @param fichFornecedor ficheiro a utilizar
+	*/
 	void guardaFornecedores(string fichFornecedores); //guarda o que esta no vetor no ficheiro de texto
+	/**
+	*	@brief Guarda as ofertas num ficheiro
+	*   @param fichOfertas ficheiro a utilizar
+	*/
 	void guardaOfertas(string fichOfertas); // ""
+	/**
+	*	@brief Guarda os clientes num ficheiro
+	*   @param fichClientesR ficheiro a utilizar
+	*   @param fichClientes ficheiro a utilizar
+	*/
 	void guardaClientes(string fichClientesR, string fichClientes);
+	/**
+	*	@brief Imprime os lucros dos fornecedores
+	*/
 	void printLucrosTotais() const;
+	/**
+	*	@brief Imprime os clientes
+	*/
 	void printClientes() const;
+		/**
+	*	@brief Imprime os fornecedores
+	*/
 	void printFornecedores() const;
+	/**
+	*	@brief Verifica se o fornecedor existe ou nao
+	*	@param NIF do fornecedor
+	*	@return true se existir, false caso contrario 
+	*/
 	bool checkFornecedorNIFBool(unsigned long NIF);
+	/**
+	*	@brief Remove reserva de um determinado cliente
+	*	@param clienteNif NIF do cliente
+	*	@param reserva numero da reserva a cancelar 
+	*/
 	void removeReserva(unsigned long clienteNIF, unsigned int reserva);
+	/**
+	*	@brief Imprime ofertas com um determinado destino 
+	*	@param destino 
+	*/
 	bool printOfertasByDestino(string destino) const;
+	/**
+	*	@brief Imprime ofertas com uma determinada data 
+	*	@param data
+	*/
 	bool printOfertasByData(Data data) const;
+	/**
+	*	@brief Imprime todas as ofertas disponiveis
+	*/
 	void printOfertas() const;
+	/**
+	*	@brief Verifica se o cliente existe ou nao e atribui o apontador correspondente a c1
+	*	@param NIF do cliente
+	*	@return true se existir, false caso contrario
+	*/
 	bool checkClienteNIF(unsigned long NIF, Cliente **c1);
+	/**
+	*	@brief Verifica se o cliente existe ou nao e atribui o apontador correspondente a f1
+	*	@param NIF do fornecedor
+	*	@return true se existir, false caso contrario
+	*/
 	bool checkFornecedorNIF(unsigned long NIF, Fornecedor **f1); //verifica se existe o fornecedor com o NIF especificado e atribui o apontador correspondente a f1
+	/**
+	*	@brief Verifica se o cliente existe ou nao, atira uma excecao caso exista
+	*	@param NIF do fornecedor
+	*/
 	void checkFornecedorNIF(unsigned long NIF); // so verifica se o fornecedor em questao existe, atira uma excecao se for esse o caso 
+	
 	void showPontosCliente(Cliente *c1);
+	/**
+	*	@brief Atribui uma oferta a um cliente
+	*	@param fornecedorNif 
+	*	@param clienteNif 
+	*	@param numeroOferta 
+	*/
 	void atribuiReserva(unsigned long fornecedorNif, unsigned long clienteNIF, int numeroOferta, Data dataReserva);
+	/**
+	*	@brief Remove um fornecedor
+	*	@param NIF do fornecedor
+ 	*/
 	void removerFornecedor(unsigned long NIF);
+	/**
+	*	@brief Imprime as reservas de um cliente
+	*	@param NIF do cliente
+	*/
 	void printReservasByCliente(unsigned long NIF) const;
 
 	//BST e reservas
