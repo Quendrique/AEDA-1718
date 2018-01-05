d2f1a92ccc8cebe9b5d527c18e9dd85028682500
catarinafigueiredo <32490017+catarinafigueiredo@users.noreply.github.com>
2018-01-05 12:33:24 +0000

    comentarios adicionados

--- reservas.h
+++ reservas.h
@@ -15,18 +15,72 @@ private:
 	string fornecedor;
 public:
 	Reserva();
+	/**
+	*	@brief Construtor de uma reserva
+	*   @param data
+	*   @param nif
+	*   @param nomecliente
+	*   @param oferta
+	*   @param fornecedor
+	*/
 	Reserva(Data data, unsigned long nif, string nomeCliente, Oferta* oferta, string fornecedor);
+	/**
+	*	@brief Devolve a data da reserva
+	*   @return data
+	*/
 	Data getData() const;
+	/**
+	*	@brief Devolve o nif 
+	*   @return nif
+	*/
 	unsigned long getNif() const;
+	/**
+	*	@brief Devolve o nome do cliente
+	*   @return nomecliente
+	*/
 	string getNomeCliente() const;
+	/**
+	*	@brief Altera o NIF 
+	*   @param nif
+	*/
 	void setNif(unsigned long Nif);
+	/**
+	*	@brief Altera o nome do cliente
+	*   @param nome
+	*/
 	void setNomeCliente(string nome);
+	/**
+	*	@brief Altera a data da reserva
+	*   @param data
+	*/
 	void setData(int dia, int mes);
+	/**
+	*	@brief Altera a oferta
+	*   @param reserva
+	*/
 	void setOferta(Oferta* oferta);
+	/**
+	*	@brief Altera o  fornecedor 
+	*   @param fornecedor
+	*/
 	void setFornecedor(string fornecedor);
+	/**
+	*	@brief Devolve a oferta
+	*   @return oferta
+	*/
 	Oferta* getOferta() const;
+	/**
+	*	@brief Devolve o fornededor
+	*   @return fornecedor
+	*/
 	string getFornecedor() const;
+	/**
+	*	@brief Overload do operador <
+	*/
 	bool operator < (const Reserva &r2) const;
+	/**
+	*	@brief Overload do operador ==
+	*/
 	bool operator == (const Reserva &r2) const;
 
 };
